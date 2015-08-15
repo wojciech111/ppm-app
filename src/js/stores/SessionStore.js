780287cf50610fc7677094465cae8d7f3e103bf7
wojciech111 <oxio22@gmail.com>
2015-08-15 08:30:03 -0700

    dodanie session store i sessionactioncreator

---
+++ src/js/stores/SessionStore.js
@@ -0,0 +1,80 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher.js');
+var AppConstants = require('../constants/AppConstants.js');
+var EventEmitter = require('events').EventEmitter;
+var objectAssign = require('react/lib/Object.assign');
+
+var ActionTypes = AppConstants.ActionTypes;
+var CHANGE_EVENT = 'change';
+
+// Load an access token from the session storage, you might want to implement
+// a 'remember me' using localSgorage
+var _accessToken = sessionStorage.getItem('accessToken')
+var _email = sessionStorage.getItem('email')
+var _errors = [];
+
+var SessionStore = objectAssign({}, EventEmitter.prototype, {
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  isLoggedIn: function() {
+    return _accessToken ? true : false;    
+  },
+
+  getAccessToken: function() {
+    return _accessToken;
+  },
+
+  getEmail: function() {
+    return _email;
+  },
+
+  getErrors: function() {
+    return _errors;
+  }
+
+});
+
+SessionStore.dispatchToken = AppDispatcher.register(function(payload) {
+  var action = payload.action;
+
+  switch(action.type) {
+
+    case ActionTypes.LOGIN_RESPONSE:
+      if (action.json && action.json.access_token) {
+        _accessToken = action.json.access_token;
+        _email = action.json.email;
+        // Token will always live in the session, so that the API can grab it with no hassle
+        sessionStorage.setItem('accessToken', _accessToken);
+        sessionStorage.setItem('email', _email);
+      }
+      if (action.errors) {
+        _errors = action.errors;
+      }
+      SessionStore.emitChange();
+      break;
+
+    case ActionTypes.LOGOUT:
+      _accessToken = null;
+      _email = null;
+      sessionStorage.removeItem('accessToken');
+      sessionStorage.removeItem('email');
+      SessionStore.emitChange();
+      break;
+
+    default:
+  }
+
+  return true;
+});
+
+module.exports = SessionStore;
